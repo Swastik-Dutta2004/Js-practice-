ca5e2d35b1dc3aa974deb0836bfd3552faacb323
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-10 21:26:58 +0530

    Some of arrays program

---
+++ basic1/array1.js
@@ -0,0 +1,30 @@
+// let mydate = new Date()
+
+// let mycreateddate = new Date (2023,0,23)
+// console.log(mycreateddate.toDateString());
+
+ 
+const myarr = new Array (1,2,3,4,5);
+console.log(myarr);
+// myarr.push(6);
+// myarr.push(7);
+// myarr.pop();
+// myarr.unshift(9);
+// myarr.shift();
+
+// console.log (myarr.includes(5))
+// console.log (myarr.indexOf(3))
+// console.log (myarr);
+
+// const newarr = myarr.join()
+
+// console.log(myarr);
+// console.log(typeof newarr);
+
+// const myNewArr1 = myarr.slice(1,3)
+// console.log (myarr);
+// console.log(myNewArr1);
+
+const myNewArr = myarr.splice(1,3)
+console.log (myarr);
+console.log (myNewArr);
