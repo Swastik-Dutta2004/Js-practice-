db9a8b160ecde4f343cdb46a9a0b7b624c436ecb
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-12 19:22:21 +0530

    second practice session on function .

--- basic1/function2.js
+++ basic1/function2.js
@@ -3,12 +3,25 @@ return num1
 }
 // console.log(calculateCartPrice(400,500,600,700));
 
-const user = {
-    username: "swastik",
-    price: 231346,
+// const user = {
+//     username: "swastik",
+//     price: 231346,
+// }
+
+// function GoodsTypes (user){
+// console.log(`user name is ${user.username} and the price is ${user.price}`);
+// }
+// return GoodsTypes(user)
+
+const handelObject = {
+    username : "saw",
+    price: 1478952
 }
 
-function GoodsTypes (anyObject){
-console.log(`user name is ${anyObject.username} and the price is ${anyObject.price}`);
+const myNewArray =[400,500,600,700,800]
+
+function returnSecondValue(russian){
+    return russian [3]
 }
-return GoodsTypes(user)
+// console.log(returnSecondValue(myNewArray));
+ console.log(returnSecondValue[400,500,600,700,800]);
