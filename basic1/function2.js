e0bf8f0ed55e5cb33af434a2b9d7b0e43fb18748
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-12 18:44:45 +0530

    second parctice session on function

---
+++ basic1/function2.js
@@ -0,0 +1,14 @@
+function calculateCartPrice (...num1){
+return num1
+}
+// console.log(calculateCartPrice(400,500,600,700));
+
+const user = {
+    username: "swastik",
+    price: 231346,
+}
+
+function GoodsTypes (anyObject){
+console.log(`user name is ${anyObject.username} and the price is ${anyObject.price}`);
+}
+return GoodsTypes(user)
