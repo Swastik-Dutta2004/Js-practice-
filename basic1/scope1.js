806a155cc214e5a9b13da00a9654202f9eb384cf
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-19 09:44:22 +0530

    scope function in Java

---
+++ basic1/scope1.js
@@ -0,0 +1,40 @@
+  // let a = 300
+// if (true){
+//     let a = 12
+//     const  b = 20 
+//     console.log("Inner: ",a);
+
+// }
+
+// console.log(a);
+// function one (){
+//     const name ="swastik"
+    
+//     function two(){
+//          const website = "youtube"
+//          console.log(name);
+//     }
+//     // console.log(website);
+//    return two()
+// }
+// return one()
+
+if(true){
+    const name =  "swastik"
+    if (name === "swastik"){
+        const website = " youtube"
+        // console.log(name + website);
+    }
+    // console.log(website);
+}
+// console.log(name);
+
+function addone (num){
+    return num + 1
+}
+console.log(addone(5))
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(5))
