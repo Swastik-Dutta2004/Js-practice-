c537f150864d143e693e07b3c06dfb87cfb72318
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-24 21:32:58 +0530

    control flow part 1

---
+++ basic1/controlflow.js
@@ -0,0 +1,16 @@
+// const temperature = 41
+// if (temperature === 41){
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("above 50");
+    
+// }
+    // const score = 150
+    // if(score>200){
+    //     let power = "fly"
+    //     console.log(`user power: ${power}`);  
+    // }
+    // else {
+    //     console.log("user has no power");
+    // }
