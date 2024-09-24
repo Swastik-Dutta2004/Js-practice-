51efcdf7de5610bda4223397d4ec622fee48d43e
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-24 22:08:46 +0530

    control flow part two.

--- basic1/controlflow.js
+++ basic1/controlflow.js
@@ -14,3 +14,29 @@
     // else {
     //     console.log("user has no power");
     // }
+    // const balance = 800
+
+    // if(balance<500){
+    //     console.log("less than 500");
+    // }
+    // else if  (balance<750){
+    //     console.log("less than 750");
+    // }
+    // else if  (balance<900){
+    //     console.log("less than 900");
+    // }
+    // else  {
+    //     console.log("less than 1200");
+    // }
+    
+    const UserLoggedIn =  true
+    const DebitCard = true
+    if(UserLoggedIn && DebitCard){
+        console.log("Allow to Buy Course");
+        
+    }
+    const LoggedInfromGoogle = false
+    const LoggedInFromEmail = true
+    if(LoggedInFromEmail ||LoggedInFromEmail){
+        console.log("User Logged In");
+     }
