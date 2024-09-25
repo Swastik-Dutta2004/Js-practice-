97f871bb50c970bbd5b354a81d62dd13ef1f5b07
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-25 09:05:01 +0530

    Switch case in Java.

---
+++ basic1/switch.js
@@ -0,0 +1,18 @@
+const month = 3 
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+        
+    default:console.log("defilt case match");
+        break;
+}
