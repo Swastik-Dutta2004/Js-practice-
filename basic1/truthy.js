b68b16a10e8336d519b54cc24c16941c83d7db4f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-25 11:31:51 +0530

    truthy, false and terniary oprator in Java

---
+++ basic1/truthy.js
@@ -0,0 +1,29 @@
+// const useremial = []
+// if (useremial) {
+//     // console.log("Got email Id");
+// }
+// else {
+//     console.log("don't hav eany email id");
+    
+// }
+
+// falsy values - false, 0, -0, null, undefind, NAN, bigint, 0n, "". 
+
+// truhty values - "0", "false", " ", [], {}, function(){}. 
+
+// if (useremial.length===0){
+//     console.log("array is empty");
+// }
+
+// const empytObj = {}
+
+//     if (Object.keys(empytObj).lenght===0 ){
+//         console.log("empty obj");
+        
+    // }
+ //terniary operation 
+  
+ const chickenroll = 120
+ chickenroll>= 100 ? console.log("more than 100"):console.log("less than 100")
+ 
+ 
