c07a2627b1b7ea9e41327a908d4b0da53a570e66
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-27 13:50:10 +0530

    Part two of High Order Array Loops .

---
+++ basic1/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js : 'JavaScript',
+    Cpp: 'c++',
+    rb: 'ruby',
+    SWift: 'swift by apple'
+}
+for (const key in myObject ) {
+    
+    //   console.log(`${key} :- ${myObject[key]}`);
+        
+    }
+const programming = ['cpp','java','react','python']
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
