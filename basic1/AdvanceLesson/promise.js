18bec4fe59babdffd6852902886718eb22b33f9c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-01-18 20:04:09 +0530

    Little bit on fetch.

--- basic1/AdvanceLesson/promise.js
+++ basic1/AdvanceLesson/promise.js
@@ -80,3 +80,27 @@ async function consumepromiseFive(){
     }
 }
 consumepromiseFive()
+
+// async function getAllUser(){
+//     try {
+//        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log('E:',error);
+        
+//     }
+// }
+    // getAllUser()
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
