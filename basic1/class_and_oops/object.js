a117ccaa33f8994abd95f26f612531665d361b7c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-01-19 19:37:25 +0530

    Operators

--- basic1/class_and_oops/object.js
+++ basic1/class_and_oops/object.js
@@ -8,3 +8,20 @@ console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 
 
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai", 25)
+const tea = new createUser("tea", 250)
+
+tea.printMe()
